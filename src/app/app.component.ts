7a2c07732be84ff676f19603d92e2e3c155cd0aa
minawagieh@gmail.com <minawagieh>
2019-01-02 00:26:31 +0200

    finishing adding SignUp page with Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   ngOnInit() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAlPPzF3z_ugAYYEeGHTNFH4133r5FXMEc',
       authDomain: 'ng-recipe-book-4ae35.firebaseapp.com'
-    })
+    });
   }
 
   onNavigate(feature: string) {
