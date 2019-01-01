f68ae66a004b6ae613b9edb78f80c45553b67007
minawagieh@gmail.com <minawagieh>
2019-01-01 16:10:07 +0200

    finishing adding http requests

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import {DataStorageService} from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageServices: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageServices.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchRecipe() {
+    this.dataStorageServices.getRecipes();
+  }
 }
